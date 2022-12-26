fd6a6f5d1d82af991bc969c1af5333d6adc035f9
Aayush <aceraayush@gmail.com>
2022-12-26 17:29:58 +0545

    search completed

--- home/urls.py
+++ home/urls.py
@@ -3,5 +3,7 @@ from django.urls import path
 
 urlpatterns = [
     path("", HomeView.as_view(),name = "home"),
+    path("category/<slug>", CategoryView.as_view(),name = "category"),
+    path("search", SearchView.as_view(),name = "search"),
 
 ]
